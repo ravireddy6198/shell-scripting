42942e657568d3b775f870446521c2ac9e7e036b
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-02 12:36:51 +0530

    conversion added

---
+++ 04-variables-username.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " Please enter username ::"
+read USERNAME
+echo  "Please enter password ::"
+read -s PASSWORD
+echo " User name is $USERNAME"
+echo " Password is $PASSWORD"
+echo " login successful"
+
+
