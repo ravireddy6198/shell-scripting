2e8714f35fcc0a8a75c0e04bfbf5d1b80a79dff5
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-02 12:06:51 +0530

    adding new file and variables added

--- 02-variables.sh
+++ 02-variables.sh
@@ -7,5 +7,5 @@ PERSON2=ramu
 
 echo "$PERSON1 :: HI $PERSON2",
 echo "$PERSON2 :: $PERSON1 Good Morning How are you "
-echo "$PERSON1 :: $PERSON2 This is Hyderabad"
+echo "${PERSON1} :: $PERSON2 This is Hyderabad"
 echo " Byeee"
