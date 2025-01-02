e3d4d3c30d36b44fb4df0f33dc7caa98f0d7f847
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-02 12:20:20 +0530

    adding new file and variables added

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,7 +5,7 @@
 PERSON1=$1   # no pace before and after equal
 PERSON2=$2
 
-echo "$PERSON1 :: HI $PERSON2",
+echo "$PERSON1 :: HI $PERSON2"
 echo "$PERSON2 :: $PERSON1 Good Morning How are you "
 echo "$PERSON1 :: $PERSON2 This is Hyderabad"
 echo " Byeee"
