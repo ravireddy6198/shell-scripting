b2c129b6e94e182a89b2df2a37f10873d789e05f
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-02 12:12:27 +0530

    adding new file and variables added

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is first line of code
+
+PERSON1=$1   # no pace before and after equal
+PERSON2=$2
+
+echo "$1 :: HI $2",
+echo "$2 :: $1 Good Morning How are you "
+echo "${$1} :: $2 This is Hyderabad"
+echo " Byeee"
