d0a7b239c2b032bcd64b08c1af1beed15cd7d1aa
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-07 21:07:42 +0530

    expense-shell-script

--- 17-backup.sh
+++ 17-backup.sh
@@ -2,12 +2,20 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
+MSG=""
 
 while read -r line
 do
 
-    echo $line
-    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    #echo $line
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "$PARTITION   : $USAGE"
+    #echo "$PARTITION   : $USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then  
+        MSG="$PARTITION exceeds :$USAGE  
+    fi
 done <<< $DISK_USAGE
+
+echo "$MSG"
+
