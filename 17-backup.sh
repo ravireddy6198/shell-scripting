e1899ae3efa393a3b5575edc7ec86ac19061ecf5
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-07 20:56:33 +0530

    expense-shell-script

--- 17-backup.sh
+++ 17-backup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
 
 while read -r line
 do
+
     echo $line
+    USAGE=$(echo $line | awk -F " " '{print $NF}')
+    echo $USAGE
 done <<< $DISK_USAGE
